674859cbb3e8d5bf7a72d5866d261554a4c18478
Jessica Poeana <jpoeana1@emich.edu>
2020-11-06 23:01:04 +0000

    Added About page, request box, and seach box

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import query_on_whoosh
+import smtplib
+import config
+import math
 
 # turn this file (app.py) into a web app
 app = Flask(__name__)
@@ -25,11 +28,30 @@ def handle_query():
 @app.route("/query_view", strict_slashes=False)
 def handle_query_view():
     query_term = request.args.get("q")
-    query_page = int(request.args.get("p"))
+    if not query_term:
+        query_term = ""
+
+    query_page_arg = request.args.get("p")
+    if not query_page_arg:
+        query_page_arg = "1"
+
+    query_page = int(query_page_arg)
     query_results = query_on_whoosh.query(query_term, current_page=query_page)
-    search_results = query_results[0]
-    return  render_template("query.html", results = search_results)
+    search_results = query_results[0] # returns a pages of the search results
+    results_cnt = int(query_results[1]) #returns a total number of the search results
+    page_cnt = math.ceil( results_cnt / 10 )
+    return  render_template("query.html", results = search_results, page_cnt=page_cnt, query_term=query_term)
     
+@app.route("/about", strict_slashes=False)
+def handle_about():
+    return render_template("about.html")
 
 
-   
+@app.route("/success", strict_slashes=False)
+def handle_request():
+    new_data = request.args.get("new_data")
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.startls()
+    server.login("jpoeana1@emich.edu", config.gmail_password)
+    server.sendmail("jpoeana1@emich.edu", "jessicapoeana@gmail.com", "request " + new_data)
+    return render_template("success.html", new_data=new_data)
